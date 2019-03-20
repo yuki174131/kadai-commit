d7fc533dac8988a93aecb678ab4159081bff9b1e
yuki.sekine <yuki31.messi10@gmail.com>
2019-03-20 15:13:33 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,7 +1,15 @@
-<!DOCTYPE html>
 <?php
-    $result="無し";
+    $result= '無し';
+    if (array_key_exists('act', $_POST)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶" ];
+        return $fortune[random_int(0, count($fortune) - 1 )];
+    }
 ?>
+<!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
