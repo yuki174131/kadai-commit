8b6106c518ec3e720da4405b3498a1230bba93f5
yuki.sekine <yuki31.messi10@gmail.com>
2019-03-20 15:02:31 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+    $result="無し";
+?>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
@@ -6,7 +9,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：無し</p>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく!</button>
         </form>
